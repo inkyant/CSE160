382315384f2fc4e9b792ccf8f7eb8810f60b2394
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2026-05-05 20:42:58 -0700

    basic uv texture

---
+++ Assignment3/Cube.js
@@ -0,0 +1,173 @@
+
+class Cube {
+    constructor() {
+        this.type = 'cube'
+        this.matrix = new Matrix4()
+        this.scale = [1, 1, 1]
+        this.color = [1, 1, 1, 1]
+        this.jointRotation = [0, 1, 0, 0]
+        this.jointPos = [0, 0, 0]
+        this.pos = [0, 0, 0]
+        this.parent = null
+
+        // texture
+        this.texture0 = null
+        this.textureLoaded = false
+    }
+
+    setImage(image) {
+        if (this.texture0 == null) {
+            this.texture0 = gl.createTexture()
+        }
+
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+
+        const img = new Image()
+        img.onload = () => {
+            gl.activeTexture(gl.TEXTURE0)
+            gl.bindTexture(gl.TEXTURE_2D, this.texture0)
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+            gl.uniform1i(uTexture0, 0);
+            this.textureLoaded = true
+        }
+
+        img.src = image
+    }
+
+    render() {
+        let vertices = [
+            -0.5, -0.5, -0.5,
+             0.5, -0.5, -0.5,
+            -0.5,  0.5, -0.5,
+
+             0.5,  0.5, -0.5,
+             0.5, -0.5, -0.5,
+            -0.5,  0.5, -0.5,
+
+
+            -0.5, -0.5, -0.5,
+             0.5, -0.5, -0.5,
+            -0.5, -0.5,  0.5,
+
+             0.5, -0.5,  0.5,
+             0.5, -0.5, -0.5,
+            -0.5, -0.5,  0.5,
+            
+
+            -0.5, -0.5, -0.5,
+            -0.5, -0.5,  0.5,
+            -0.5,  0.5, -0.5,
+
+            -0.5,  0.5,  0.5,
+            -0.5, -0.5,  0.5,
+            -0.5,  0.5, -0.5,
+
+
+            -0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5, -0.5,
+
+            -0.5,  0.5,  0.5,
+             0.5,  0.5,  0.5,
+             0.5,  0.5, -0.5,
+
+
+             0.5, -0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5,  0.5, -0.5,
+            
+             0.5, -0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5, -0.5,  0.5,
+
+            
+            -0.5, -0.5, 0.5,
+             0.5, -0.5, 0.5,
+            -0.5,  0.5, 0.5,
+
+             0.5,  0.5, 0.5,
+             0.5, -0.5, 0.5,
+            -0.5,  0.5, 0.5,
+        ]
+
+        gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, currentViewMatrix.elements);
+        
+        let m = new Matrix4()
+ 
+        if (this.parent != null) {
+            m.concat(this.parent.matrix)
+        }
+
+        m.translate(this.pos[0] - this.jointPos[0], this.pos[1] - this.jointPos[1], this.pos[2] - this.jointPos[2])
+        m.rotate(...this.jointRotation)
+        m.translate(...this.jointPos)
+
+        this.matrix = new Matrix4(m)
+
+        m.scale(this.scale[0], this.scale[1], this.scale[2])
+
+        gl.uniformMatrix4fv(u_ModelMatrix, false, m.elements);
+
+        // Create a buffer object
+        let vertexBuffer = gl.createBuffer();
+        if (!vertexBuffer) {
+            console.log('Failed to create the vertex buffer object');
+            return -1;
+        }
+        // Bind the buffer object to target
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+        gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_Position);
+
+        let uvs = new Float32Array([
+            // FRONT
+            0,0, 1,0, 0,1, 1,1, 1,0, 0,1,
+            // BOTTOM
+            0,0, 1,0, 0,1, 1,1, 1,0, 0,1,
+            // LEFT
+            1,0, 0,0, 1,1, 0,1, 0,0, 1,1,
+            // TOP
+            0,1, 0,0, 1,0,  0,1, 1,1, 1,0,
+            // RIGHT
+            0,0, 1,1, 0,1, 0,0, 1,1, 1,0,
+            // BACK
+            0,0, 1,0, 0,1, 1,1, 1,0, 0,1,
+        ]);
+        let uvBuffer = gl.createBuffer();
+        if (!uvBuffer) {
+            console.log("Failed to create the uv buffer object");
+            return -1;
+        }
+        gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, uvs, gl.DYNAMIC_DRAW);
+        gl.vertexAttribPointer(a_uv, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_uv);
+
+        // Draw
+        let r = this.color[0];
+        let g = this.color[1];
+        let b = this.color[2];
+        let a = this.color[3];
+        gl.uniform4f(u_FragColor, r, g, b, a);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        gl.uniform4f(u_FragColor, 0.9*r, 0.9*g, 0.9*b, a);
+        gl.drawArrays(gl.TRIANGLES, 6, 6);
+
+        gl.uniform4f(u_FragColor, 0.8*r, 0.8*g, 0.8*b, a);
+        gl.drawArrays(gl.TRIANGLES, 12, 6);
+
+        gl.uniform4f(u_FragColor, 0.75*r, 0.75*g, 0.75*b, a);
+        gl.drawArrays(gl.TRIANGLES, 18, 6);
+        
+        gl.uniform4f(u_FragColor, 0.95*r, 0.95*g, 0.95*b, a);
+        gl.drawArrays(gl.TRIANGLES, 24, 6);
+        
+        gl.uniform4f(u_FragColor, 0.7*r, 0.7*g, 0.7*b, a);
+        gl.drawArrays(gl.TRIANGLES, 30, 6);
+    }
+
+}
---
+++ Assignment3/asg3.js
@@ -0,0 +1,188 @@
+
+// Vertex shader program
+var VSHADER_SOURCE = `
+attribute vec4 a_Position;
+uniform mat4 u_ModelMatrix;
+uniform mat4 u_GlobalRotateMatrix;
+attribute vec2 a_uv;
+varying vec2 vUv;
+
+void main() {
+  gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+
+  vUv = a_uv;
+}
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+precision mediump float;
+uniform vec4 u_FragColor;
+uniform sampler2D uTexture0;
+varying vec2 vUv;
+void main() {
+  vec4 image0 = texture2D(uTexture0, vUv);
+  gl_FragColor = 0.5*image0 + 0.5*u_FragColor;
+}
+`;
+
+// global vars
+let canvas;
+/** @type {WebGLRenderingContext} */
+let gl;
+let a_Position;
+let u_FragColor;
+let u_ModelMatrix;
+let u_GlobalRotateMatrix;
+let a_uv;
+let uTexture0;
+
+let currentViewMatrix = new Matrix4()
+
+let globalYaw = 0
+let globalPitch = 0
+let canvas_drag_sensitivity = 0.7
+
+function main() {
+    setupWebGL()
+    connectVariablesToGLSL()
+    addActionsForHtmlUI()
+
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0.1, 0.5, 0.2, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    currentViewMatrix.setRotate(globalPitch, 1, 0, 0)
+    currentViewMatrix.rotate(globalYaw, 0, 1, 0)
+    renderPage()
+    setTimeout(renderPage, 10)
+}
+
+function addActionsForHtmlUI() {
+    
+    canvas.onmousedown = (e) => {
+
+        if (e.shiftKey && !specialAnim) {
+            specialAnim = true
+        }
+
+        canvas_x_drag_init = e.clientX;
+        canvas_y_drag_init = e.clientY;
+        canvas_x_drag_prev = e.clientX;
+        canvas_y_drag_prev = e.clientY;
+        canvas_drag_last_poll = performance.now();
+        
+
+        global_yaw_init = globalYaw;
+        global_pitch_init = globalPitch;
+        canvas_x_drag_vel = canvas_y_drag_vel = 0;
+
+        renderPage();
+
+        canvas.onmousemove = (e) => {
+            const total_x_delta = e.clientX - canvas_x_drag_init;
+            const total_y_delta = e.clientY - canvas_y_drag_init;
+
+            const t_delta = performance.now() - canvas_drag_last_poll;
+            canvas_x_drag_vel = (e.clientX - canvas_x_drag_prev) / t_delta; // delta per ms
+            canvas_y_drag_vel = (e.clientY - canvas_y_drag_prev) / t_delta; // delta per ms
+
+            canvas_x_drag_vel =
+                Math.sign(canvas_x_drag_vel) *
+                Math.sqrt(Math.abs(canvas_x_drag_vel * 0.1)) *
+                0.9;
+            canvas_y_drag_vel =
+                Math.sign(canvas_y_drag_vel) *
+                Math.sqrt(Math.abs(canvas_y_drag_vel * 0.1)) *
+                0.9;
+
+            canvas_x_drag_prev = e.clientX;
+            canvas_y_drag_prev = e.clientY;
+            canvas_drag_last_poll = performance.now();
+
+            globalYaw =
+                global_yaw_init - total_x_delta * canvas_drag_sensitivity;
+            globalPitch =
+                global_pitch_init - total_y_delta * canvas_drag_sensitivity;
+
+            currentViewMatrix.setRotate(globalPitch, 1, 0, 0)
+            currentViewMatrix.rotate(globalYaw, 0, 1, 0)
+            renderPage();
+        };
+        document.onmouseup = () => {
+            canvas.onmousemove = null;
+        };
+    };
+}
+
+function renderPage() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    let c = new Cube()
+    c.setImage("uvCoords.png")
+    c.render()
+}
+
+const connectVariablesToGLSL = () => {
+    // Initialize shaders
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    // Get the storage location of a_Position
+    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    // Get the storage location of u_FragColor
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (!u_FragColor) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+
+    // Get the storage location of u_ModelMatrix
+    u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+    if (!u_ModelMatrix) {
+        console.log('Failed to get the storage location of u_ModelMatrix');
+        return;
+    }
+
+    // Get the storage location of u_GlobalRotateMatrix
+    u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+    if (!u_GlobalRotateMatrix) {
+        console.log('Failed to get the storage location of u_GlobalRotateMatrix');
+        return;
+    }
+
+    // Get the storage location of a_uv
+    a_uv = gl.getAttribLocation(gl.program, 'a_uv');
+    if (a_uv < 0) {
+        console.log('Failed to get the storage location of a_uv');
+        return;
+    }
+
+    uTexture0 = gl.getUniformLocation(gl.program, "uTexture0")
+    if (uTexture0 < 0) {
+        console.warn("could not find uTexture0 location")
+    }
+
+}
+
+const setupWebGL = () => {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = canvas.getContext("webgl", { preserveDrawingBuffer: true })
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+}
